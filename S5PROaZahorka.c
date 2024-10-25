2b87e9f145bdc0c8b41fc4e59ba7730e9bf8d10c
Augustin Zahorka <augustin.zahorka@gmail.com>
2024-10-25 13:47:01 +0200

    Ceci est le premier test de GitHub

--- S5PROaZahorka.c
+++ S5PROaZahorka.c
@@ -60,6 +60,7 @@ void menu_principal(char select[3]);
 int est_coordonnees(char select[3])
 {
     return strlen(select) == 3 && isdigit(select[0]) && select[1] == '-' && isdigit(select[2]);
+    printf("Test : Git");
 }
 
 Navire creer_navire(int indice_navire, Joueur *joueur, char select[3], bool aleatoire)
